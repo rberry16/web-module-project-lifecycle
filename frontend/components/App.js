d068fbae92bbf76e8d713994b43a1063c40ce791
Ryan Berry <rberry16@hotmail.com>
2022-04-13 14:06:07 -0700

    ul built in state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,15 +3,34 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    const {todos} = this.state;
     return (
       <div>
         <h1>Hello</h1>
         
         <ul>
-          <li>Rock</li>
-          <li>The</li>
-          <li>House</li>
+          {todos.map(todo => {
+            return <li key={todo.id}>{todo.name} {todo.completed ? <span>- completed</span> : <span></span>}</li>
+          })}
         </ul>
 
         <form>
